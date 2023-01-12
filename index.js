343f2d9b211261babe0f7ba753b59ea22e2517e0
unknown <apu.islam@g.bracu.ac.bd>
2023-01-12 20:48:19 +0600

    combine reducers

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const redux = require("redux")
 const bindActionCreators = redux.bindActionCreators
+const combineReducers = redux.combineReducers
 const createStore = redux.createStore
 
 const CAKE_ORDERED = "CAKE_ORDERED"
 const BUY_CAKE = "BUY_CAKE"
+const ORDER_ICECREAM = "ORDER_ICECREAM"
+const BUY_ICECREAM = "BUY_ICECREAM"
 
 function orderCake(){
     return {
@@ -19,11 +22,46 @@ function buyCake(payload=2){
     }
 }
 
-const initialState = {
-    numOfCakes:10,
+function orderIceCream(payload=1){
+    return {
+        type: ORDER_ICECREAM,
+        payload:payload,
+    }
 }
 
-const reducer = (state=initialState,action)=>{
+function buyIceCream(payload=1){
+    return {
+        type: BUY_ICECREAM,
+        payload:payload,
+    }
+}
+
+const initialCakeState = {
+    numOfCakes:10,
+}
+const initialIceCreamState={
+    numOfIceCream:12
+}
+
+const iceCreamReducer = (state=initialIceCreamState,action)=>{
+    switch(action.type){
+        case ORDER_ICECREAM:
+            return{
+                ...state,
+                numOfIceCream:state.numOfIceCream-action.payload
+            }
+        case BUY_ICECREAM:
+            return{
+                ...state,
+                numOfIceCream:state.numOfIceCream+action.payload
+            }
+        default:
+            return state
+
+    }
+}
+
+const cakeReducer = (state=initialCakeState,action)=>{
     switch(action.type){
         case CAKE_ORDERED:
             return{
@@ -32,7 +70,7 @@ const reducer = (state=initialState,action)=>{
             }
             // break;
         case BUY_CAKE:
-            return {
+            return{
                 ...state,
                 numOfCakes:state.numOfCakes+action.payload,
             }
@@ -41,7 +79,42 @@ const reducer = (state=initialState,action)=>{
             return state
     }
 }
-const store = createStore(reducer)
+
+// const reducer = (state=initialState,action)=>{
+//     switch(action.type){
+//         case CAKE_ORDERED:
+//             return{
+//                 ...state,
+//                 numOfCakes: state.numOfCakes-action.payload,
+//             }
+//             // break;
+//         case BUY_CAKE:
+//             return {
+//                 ...state,
+//                 numOfCakes:state.numOfCakes+action.payload,
+//             }
+//             // break
+//         case ORDER_ICECREAM:
+//             return{
+//                 ...state,
+//                 numOfIceCream:state.numOfIceCream-action.payload
+//             }
+//         case BUY_ICECREAM:
+//             return{
+//                 ...state,
+//                 numOfIceCream:state.numOfIceCream+action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
+
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    iceCream:iceCreamReducer,
+
+})
+const store = createStore(rootReducer)
 
 console.log("initial state", store.getState())
 const unsubscribe = store.subscribe(()=>console.log("Updated State:",store.getState()))
@@ -49,9 +122,12 @@ const unsubscribe = store.subscribe(()=>console.log("Updated State:",store.getSt
 // store.dispatch(orderCake())
 // store.dispatch(buyCake())
 
-const actions = bindActionCreators({orderCake,buyCake},store.dispatch)
+const actions = bindActionCreators({orderCake,buyCake,orderIceCream,buyIceCream},store.dispatch)
 actions.orderCake()
 actions.orderCake()
 actions.buyCake()
+actions.orderIceCream()
+actions.orderIceCream()
+actions.buyIceCream()
 unsubscribe()
 store.dispatch(orderCake())
