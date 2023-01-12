82fee015b413800d1ebcbb633c6c6c5d7fb81b1e
unknown <apu.islam@g.bracu.ac.bd>
2023-01-12 13:57:17 +0600

    update

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const redux = require("redux")
 const createStore = redux.createStore
 
 const CAKE_ORDERED = "CAKE_ORDERED"
+const BUY_CAKE = "BUY_CAKE"
 
 function orderCake(){
     return {
         type: CAKE_ORDERED,
+        quantity:1,
+    }
+}
+
+function buyCake(){
+    return{
+        type:BUY_CAKE,
         quantity:1,
     }
 }
@@ -22,16 +30,23 @@ const reducer = (state=initialState,action)=>{
                 ...state,
                 numOfCakes: state.numOfCakes-1,
             }
+            // break;
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes:state.numOfCakes+1,
+            }
+            // break
         default:
-            return state 
+            return state
     }
 }
 const store = createStore(reducer)
 
 console.log("initial state", store.getState())
-const unsubscribe = store.subscribe(()=>console.log("Updated State",store.getState()))
+const unsubscribe = store.subscribe(()=>console.log("Updated State:",store.getState()))
 store.dispatch(orderCake())
 store.dispatch(orderCake())
-store.dispatch(orderCake())
+store.dispatch(buyCake())
 unsubscribe()
 store.dispatch(orderCake())
