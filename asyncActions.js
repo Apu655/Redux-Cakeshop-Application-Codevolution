ac58705d1cccfc25aa5d4f110a2144d6cf5945d7
unknown <apu.islam@g.bracu.ac.bd>
2023-01-22 15:29:50 +0600

    asynch actions

--- asyncActions.js
+++ asyncActions.js
@@ -1,3 +1,10 @@
+const redux = require("redux")
+const applyMiddleware = redux.applyMiddleware
+const createStore = redux.createStore
+const thunkMiddleware = require("redux-thunk").default
+const axios = require("axios")
+
+
 initialState = {
     loading:true,
     users:[],
@@ -26,3 +33,44 @@ const fetchUsersFailure = error =>{
         payload: error
     }
 }
+
+const reducer = (state=initialState,action)=>{
+    switch(action.type){
+        case FETCH_USERS_REQUEST:
+            return {
+                ...state,
+                loading:true
+            }
+        case FETCH_USERS_SUCCESS:
+            return {
+                loading:false,
+                users:action.payload,
+                error:""
+            }
+        case FETCH_USERS_FAILURE:
+            return{
+                loading:true,
+                users:[],
+                payload:error,
+            }
+    }
+}
+
+const fetchUsers = ()=>{
+    return function(dispatch){
+        dispatch(fetchUsersRequest())
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(res=>{
+            // response.data is the array of users
+            const users = res.data.map(user=>user.id)
+            dispatch(fetchUsersSuccess(users))
+        })
+        .catch(error=>{
+            // error.message gives the description of the error.
+        })
+    }
+}
+
+const store = createStore(reducer,applyMiddleware(thunkMiddleware))
+const unsubscribe = store.subscribe(()=>{console.log("Current State",store.getState())})
+store.dispatch(fetchUsers())
