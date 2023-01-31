3b5a208ef50d9287d762ca5d476cc8a17265772e
unknown <apu.islam@g.bracu.ac.bd>
2023-01-31 21:49:17 +0600

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import rootReducer from "./rootReducer";
 import logger from 'redux-logger'
 
 
-
+console.log("Store is working")
 const store = createStore(rootReducer,applyMiddleware(logger))
 
 export default store;
