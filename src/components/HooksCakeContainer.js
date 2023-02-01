e689c47cf86d7c20bb5b34b94ad6ddc3721e90aa
unknown <apu.islam@g.bracu.ac.bd>
2023-02-01 12:44:39 +0600

    update

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector,useDispatch } from 'react-redux'
 import {buyCake} from "../redux"
 const HooksCakeContainer = () => {
     const {numOfCakes} = useSelector((state)=>state.cake)
     const dispatch = useDispatch()
+    const [cakeNum,setCakeNum] = useState(1)
   return (
     <div>
+      <input type='text' onChange={(e)=>setCakeNum(parseInt(e.target.value)) } value={cakeNum}/>
         <h2>Num of cakes - {numOfCakes} </h2>
-        <button onClick={()=>dispatch(buyCake())}>Buy Cake</button>
+        <button onClick={()=>dispatch(buyCake(cakeNum))}>Buy Cake {cakeNum} cakes</button>
     </div>
   )
 }
